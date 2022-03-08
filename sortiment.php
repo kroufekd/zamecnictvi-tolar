52b78311adf7d0c96127002c971ec7efc8747bc1
kroufekd <kroufekd@gmail.com>
2022-03-08 09:42:31 +0100

    gallery

--- sortiment.php
+++ sortiment.php
@@ -17,6 +17,11 @@
                 <div class="block-heading">
                     <h2 class="text-info">Sortiment</h2>
                 </div>
+                <h3>Ploty</h3>
+                <h3>Brány</h3>
+                <h3>Schody</h3>
+                <h3>Balkónové oplocení</h3>
+                <h3>Stínící technika</h3>
             </div>
         </section>
        
